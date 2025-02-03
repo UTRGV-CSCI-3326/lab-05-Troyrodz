4e3e8d79644dca2f8b0332d64c7bdc1217639021
Troyrodz <troy.rodriguez02@utrgv.edu>
2025-02-03 03:40:37 +0000

    Final

--- src/Input.java
+++ src/Input.java
@@ -1,1 +1,33 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        // makes scanner
+        Scanner scanner = new Scanner(System.in);
+
+        // Get the users name
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        // Get user age
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        // Get user weiught
+        System.out.print("Enter your weight: ");
+        double weight = scanner.nextDouble();
+
+        // Get bool
+        System.out.print("Are you a smoker?: ");
+        boolean isSmoker = scanner.nextBoolean();
+
+        // Output
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Weight: " + weight);
+        System.out.println("Smoker: " + isSmoker);
+
+        scanner.close();
+    }
+}
 
